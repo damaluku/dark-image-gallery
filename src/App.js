bed1a3249da8b6368a7277cc0da25499dcc0f128
damaluku <damaluku@gmail.com>
2022-04-26 14:40:42 +0100

    first commit for image gallery

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import ImageCard from "./components/ImageCard";
+import ImageSearch from "./components/ImageSearch";
 
 function App() {
+  const [images, setimages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setimages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+      });
+  }, [term]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <ImageSearch searchText={(text) => setTerm(text)} />
+
+        {!isLoading && images.length === 0 && <h1>No images found</h1>}
+
+        {isLoading ? (
+          <h1>Loading...</h1>
+        ) : (
+          <div className="grid-container">
+            {images.map((image) => {
+              return <ImageCard key={image.id} image={image} />;
+            })}
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
